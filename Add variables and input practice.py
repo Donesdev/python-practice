dfc47781e0bbab05574417f1ab27e119ed63a8d3
Jose Dones <donesdev@gmail.com>
2026-01-16 13:31:04 -0500

    regex_input_practice.py
    Add regex input validation practice

---
+++ Add variables and input practice.py
@@ -0,0 +1,24 @@
+name = input("What's your name ?")
+age = input("How old are you ?")
+
+print("Hello", name)
+print("You are", age, "Years old")
+
+
+
+name = input("What's your name ?")
+age = int(input("How old are you ?"))
+
+print("Hello", name)
+print("You are", age, "Years old")
+
+
+
+import re
+name = input("What's your name ? ")
+age_text = input("How old are you ?")
+age_match = re.search(r"\d+", age_text)
+age = int(age_match.group() if age_match else "unknown")
+
+print (f"Hello {name}")
+print(f"You are {age} years old")
