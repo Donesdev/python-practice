e3adac5bfeb4fda87d2231b488ec0b260b27406b
Jose Dones <donesdev@gmail.com>
2026-01-20 10:52:09 -0500

    Update Add variables and input practice.py

--- Add variables and input practice.py
+++ Add variables and input practice.py
@@ -63,5 +63,22 @@ if h > 40:
     otpay = regularpay + otrate
     print(otpay)
 
+# Grade score 
+score = input("Enter grade score between 0.0 and 1.0: ")
+scoree = float(score)
+if scoree >= 0.9:
+    print("A")
+elif scoree >= 0.8:
+    print("B")
+elif scoree >= 0.7:
+    print("C")
+elif scoree >= 0.6:
+    print("D")
+elif scoree < 0.6:
+    print("F")
+else:
+    print("Enter numeric input between 0.0 and 1.0")
+    
+
     
    
