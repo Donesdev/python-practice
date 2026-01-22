b4b2075800de092fae9a37ce3073e0cb07b0a547
Jose Dones <donesdev@gmail.com>
2026-01-21 20:42:52 -0500

    Update Add variables and input practice.py

--- Add variables and input practice.py
+++ Add variables and input practice.py
@@ -164,4 +164,12 @@ print("Maximum is", largest)
 print("Minimum is", smallest)
 
     
-   
+#    Write a program that prompts for a file name, then opens that file and reads through the file, and print the contents of the file in upper case. Use the file words.txt to produce the output below.
+# You can download the sample data at http://www.py4e.com/code3/words.txt
+
+# Use words.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname.rstrip())
+inp = fh.read()
+print(inp.upper())
+    
